5b5f801488ae30bcdc7bb179eea418465f2fee83
Vijayan Sankar <svijayan01@gmail.com>
2025-08-11 19:00:23 +0530

    bfs done

---
+++ 2_bfs.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include "graph.h"
+#include<queue>
+#include<set>
+using namespace std;
+
+void BFS(Graph &g, string start, string goal) {
+    queue<vector<string>> q;
+    set<string> visited;
+
+    q.push({start});
+    visited.insert(start);
+
+    while (!q.empty()) {
+        vector<string> path = q.front();
+        q.pop();
+
+        string node = path.back();
+
+        if (node == goal) {
+            cout << "BFS Path found: ";
+            for (auto &p : path) cout << p << " ";
+            cout << "\n";
+            return;
+        }
+
+        for (auto &neighbor : g.adj[node]) {
+            if (!visited.count(neighbor)) {
+                visited.insert(neighbor);
+                vector<string> newPath = path;
+                newPath.push_back(neighbor);
+                q.push(newPath);
+            }
+        }
+    }
+}
+
+int main() {
+    Graph g = createGraph();
+    BFS(g, "S", "G");
+    return 0;
+}
