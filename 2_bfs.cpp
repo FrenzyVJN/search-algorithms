d5dd0902dd934842e73f36f4d369676fe5e42640
Vijayan Sankar <svijayan01@gmail.com>
2025-08-11 19:00:58 +0530

    used prettier for formatting

--- 2_bfs.cpp
+++ 2_bfs.cpp
@@ -1,31 +1,37 @@
-#include<iostream>
+#include <iostream>
 #include "graph.h"
-#include<queue>
-#include<set>
+#include <queue>
+#include <set>
 using namespace std;
 
-void BFS(Graph &g, string start, string goal) {
+void BFS(Graph &g, string start, string goal)
+{
     queue<vector<string>> q;
     set<string> visited;
 
     q.push({start});
     visited.insert(start);
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         vector<string> path = q.front();
         q.pop();
 
         string node = path.back();
 
-        if (node == goal) {
+        if (node == goal)
+        {
             cout << "BFS Path found: ";
-            for (auto &p : path) cout << p << " ";
+            for (auto &p : path)
+                cout << p << " ";
             cout << "\n";
             return;
         }
 
-        for (auto &neighbor : g.adj[node]) {
-            if (!visited.count(neighbor)) {
+        for (auto &neighbor : g.adj[node])
+        {
+            if (!visited.count(neighbor))
+            {
                 visited.insert(neighbor);
                 vector<string> newPath = path;
                 newPath.push_back(neighbor);
@@ -35,7 +41,8 @@ void BFS(Graph &g, string start, string goal) {
     }
 }
 
-int main() {
+int main()
+{
     Graph g = createGraph();
     BFS(g, "S", "G");
     return 0;
